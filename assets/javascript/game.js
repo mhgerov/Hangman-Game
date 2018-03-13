0e740cab8cae25b81f9939ac5415c2f62dee1727
Michael Gerov <mhgerov@gmail.com>
2018-03-13 08:37:17 -0400

    rename pics; add event listener

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,40 @@
-var mysteryWord = "Chameleon";
+//Declare variables
+var mysteryWordList = ["Chameleon","Badger","Porcupine","Platypus"]
+var mysteryWord;
+var wins, losses = 0;
+var lettersGuessed = [];
+var numTries = 9;
+var input = "";
 
-var x = document.getElementById("board").innerHTML;
+document.addEventListener('keydown', function(event) {
+	document.getElementById("input").innerHTML = event.key;
+});
+
+//main loop
+init();
+
+//function declarations
+function init() {
+	mysteryWord = mysteryWordList[Math.floor(Math.random()*mysteryWordList.length)];
+	document.getElementById("board").innerHTML = hideWord(mysteryWord);
+	document.getElementById("lettersGuessed").innerHTML = "None";
+	numTries = 9;
+	setImg(9);
+	input = "";
+	document.getElementById("input").innerHTML = input;
+}
 
 // "Word" returns "_ _ _ _"
 function hideWord(word) {
-	return word;
+	var hidden = "";
+	for (var i=0;i<word.length-1;i++) {
+		hidden+="_ "
+	}
+	hidden+="_";
+	return hidden;
 }
+
+function setImg(num) {
+	document.getElementById("hangman").src="assets/images/hangman"+num+".jpg";
+}
+
